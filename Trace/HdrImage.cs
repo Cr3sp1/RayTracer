434a9b699b1b188835f3155976c07ebaec53b59c
giorgiaiori <giorgia.iori@studenti.unimi.it>
2025-03-12 23:19:38 +0100

    feat: create HdrImage class

--- Trace/HdrImage.cs
+++ Trace/HdrImage.cs
@@ -2,5 +2,22 @@
 
 public class HdrImage
 {
+    // HdrImage fields
+    public int width, height;
+    public Color[] pixels;
 
+    // Constructor
+    public HdrImage(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        this.pixels = new Color[width * height];
+    }
+
+    // Get a pixel
+    public Color GetPixel(int x, int y) => pixels[y * width + x];
+    
+    // Set a pixel
+    public Color SetPixel(int x, int y, Color c) => pixels[y * width + x] = c;
+    
 }
