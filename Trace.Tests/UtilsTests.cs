6874b363abfbb439c8870c362a1ffbbf465f58f5
Cristian <cristiancrespi1@gmail.com>
2025-03-13 20:25:42 +0100

    feat: add unit test to Utils

---
+++ Trace.Tests/UtilsTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Trace;
+
+namespace Trace.Tests;
+
+public class UtilsTests
+{
+    [Fact]
+    public void TestCloseEnough()
+    {
+        Assert.True(Utils.CloseEnough(1f, 1f + 0.9e-5f));
+        Assert.True(Utils.CloseEnough(100, 101, 2));
+        Assert.False(Utils.CloseEnough(1f, 1f - 2e-5f));
+    }
+}
