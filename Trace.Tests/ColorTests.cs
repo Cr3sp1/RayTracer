245dd4f4c111338fe59978543e5e2d838753f5d5
giorgiaiori <giorgia.iori@studenti.unimi.it>
2025-03-13 12:24:01 +0100

    Add unit tests for Color

---
+++ Trace.Tests/ColorTests.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+using Trace;
+
+namespace Trace.Tests;
+
+public class ColorTests
+{
+    // Test equality between two Colors
+    [Fact]
+    public void TestCloseEnough()
+    {
+        Color col1 = new Color(1.0f, 2.0f, 3.0f);
+
+        Assert.True(Color.CloseEnough(new Color(1.0f, 2.0f, 3.0f), col1));
+    }
+
+    // Test sum between two Colors
+    [Fact]
+    public void TestAdd()
+    {
+        Color col1 = new Color(1.0f, 2.0f, 3.0f);
+        Color col2 = new Color(5.0f, 6.0f, 7.0f);
+
+        Assert.True(Color.CloseEnough(new Color(6.0f, 8.0f, 10.0f), col1 + col2));
+    }
+
+    // Test product between scalar and Color or Color and Color
+    [Fact]
+    public void TestProd()
+    {
+        Color col1 = new Color(1.0f, 2.0f, 3.0f);
+        Color col2 = new Color(5.0f, 6.0f, 7.0f);
+        float m = 4.0f;
+
+        Assert.True(Color.CloseEnough(new Color(4.0f, 8.0f, 12.0f), m * col1));
+        Assert.True(Color.CloseEnough(new Color(5.0f, 12.0f, 21.0f), col1 * col2));
+    }
+}
