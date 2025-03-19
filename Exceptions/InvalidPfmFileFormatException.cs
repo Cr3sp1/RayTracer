260976678cd51e8a0b2ec5fd044967369c02bbe2
laptopC <cristiancrespi1@gmail.com>
2025-03-19 11:43:06 +0100

    feat: add Exception class library containing InvalidPfmFileFormatException and TestFiles folder

---
+++ Exceptions/InvalidPfmFileFormatException.cs
@@ -0,0 +1,18 @@
+﻿namespace Exceptions;
+
+using System;
+
+public class InvalidPfmFileFormatException : FormatException
+{
+    public InvalidPfmFileFormatException() : base("Invalid Pfm file format.")
+    {
+    }
+
+    public InvalidPfmFileFormatException(string message) : base(message)
+    {
+    }
+
+    public InvalidPfmFileFormatException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
