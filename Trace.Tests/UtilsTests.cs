076f24ec88a9433f438873a5352c1cc5e0749e93
giorgiaiori <giorgia.iori@studenti.unimi.it>
2025-03-21 17:34:53 +0100

    feat: add ParseImgSize function with its unit tests

--- Trace.Tests/UtilsTests.cs
+++ Trace.Tests/UtilsTests.cs
@@ -1,4 +1,5 @@
 using System;
+using Exceptions;
 using Xunit;
 using Trace;
 
@@ -14,14 +15,85 @@ public class UtilsTests
         Assert.True(Utils.CloseEnough(100, 101, 2));
         Assert.False(Utils.CloseEnough(1f, 1f - 2e-5f));
     }
-    
+
     // Tests for methods regarding PFM format
-    
+
     // Method ParseEndianness
     [Fact]
     public void TestParseEndianness()
     {
-        Assert.True(Utils.ParseEndianness("1.0")==Utils.Endianness.BigEndian);
-        Assert.True(Utils.ParseEndianness("-1.0")==Utils.Endianness.LittleEndian);
+        Assert.True(Utils.ParseEndianness("1.0") == Utils.Endianness.BigEndian);
+        Assert.True(Utils.ParseEndianness("-1.0") == Utils.Endianness.LittleEndian);
+
+        /*try
+        {
+            var e = new Utils.Endianness();
+            e = Utils.ParseEndianness("0.0");
+            Assert.Fail("Expected exception");
+        }
+        catch (InvalidPfmFileFormatException)
+        {
+            Assert.True(true);
+        }*/
+
+        try
+        {
+            var en = new Utils.Endianness();
+            en = Utils.ParseEndianness("pippo");
+            Assert.Fail("Expected exception");
+        }
+        catch (InvalidPfmFileFormatException)
+        {
+            Assert.True(true);
+        }
+    }
+
+    // Method ParseImgSize
+    [Fact]
+    public void TestParseImgSize()
+    {
+        Assert.True(Utils.ParseImgSize("3 2") == (3, 2));
+        Assert.False(Utils.ParseImgSize("1 2") == (3, 2));
+
+        int width, height;
+        try
+        {
+            (width, height) = Utils.ParseImgSize("1 2 3");
+            Assert.Fail("Expected exception for invalid length of line");
+        }
+        catch (InvalidPfmFileFormatException)
+        {
+            Assert.True(true);
+        }
+
+        try
+        {
+            (width, height) = Utils.ParseImgSize("1");
+            Assert.Fail("Expected exception for invalid length of line");
+        }
+        catch (InvalidPfmFileFormatException)
+        {
+            Assert.True(true);
+        }
+
+        try
+        {
+            (width, height) = Utils.ParseImgSize("-1 1");
+            Assert.Fail("Expected exception for negative width or height");
+        }
+        catch (InvalidPfmFileFormatException)
+        {
+            Assert.True(true);
+        }
+
+        try
+        {
+            (width, height) = Utils.ParseImgSize("pippo pippa");
+            Assert.Fail("Expected exception for invalid width or height");
+        }
+        catch (InvalidPfmFileFormatException)
+        {
+            Assert.True(true);
+        }
     }
 }
--- Trace/Utils.cs
+++ Trace/Utils.cs
@@ -5,35 +5,36 @@ using Exceptions;
 public static class Utils
 {
     // Checks if two floats are equal with a tolerance epsilon
-    public static bool CloseEnough(float a, float b, float epsilon=1e-5f) => Math.Abs(a - b) < epsilon;
-    
+    public static bool CloseEnough(float a, float b, float epsilon = 1e-5f) => Math.Abs(a - b) < epsilon;
+
     // Utilities for managing PFM format
-    
+
     // Endianness
-    public enum Endianness {
+    public enum Endianness
+    {
         BigEndian = 0,
         LittleEndian = 1
     }
-    
-    // Get endianness
+
+    // Read endianness
     public static Endianness ParseEndianness(string line)
     {
         var value = 0f;
-        
+
         try
         {
             value = Convert.ToSingle(line);
         }
-        catch
+        catch (FormatException)
         {
-            throw new InvalidPfmFileFormatException();
+            throw new InvalidPfmFileFormatException("No endianness found.");
         }
 
         if (value > 0f)
         {
             return Endianness.BigEndian;
         }
-        else if(value < 0f)
+        else if (value < 0f)
         {
             return Endianness.LittleEndian;
         }
@@ -42,5 +43,31 @@ public static class Utils
             throw new InvalidPfmFileFormatException("Invalid endianness: it cannot be zero.");
         }
     }
-    
+
+    // Read width and height of the image
+    public static (int, int) ParseImgSize(string line)
+    {
+        int width, height = 0;
+        string[] elements = line.Split(' ');
+        if (elements.Length != 2)
+        {
+            throw new InvalidPfmFileFormatException("No image size found.");
+        }
+
+        try
+        {
+            width = Convert.ToInt32(elements[0]);
+            height = Convert.ToInt32(elements[1]);
+            if (width < 0 || height < 0)
+            {
+                throw new InvalidPfmFileFormatException("Invalid image siz: cannot be negative.");
+            }
+        }
+        catch (FormatException)
+        {
+            throw new InvalidPfmFileFormatException("Invalid image size");
+        }
+
+        return (width, height);
+    }
 }
