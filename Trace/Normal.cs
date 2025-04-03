d2d9ff486f0692e47b8e02dc9940218b5d2a739f
giorgiaiori <giorgia.iori@studenti.unimi.it>
2025-04-03 23:15:59 +0200

    Add Point operations and unit tests; add Normal operations

--- Trace/Normal.cs
+++ Trace/Normal.cs
@@ -6,7 +6,51 @@ namespace Trace;
 public struct Normal
 {
     public float X, Y, Z;
-    
+
     // Constructor
     public Normal(float x, float y, float z) => (X, Y, Z) = (x, y, z);
+
+    // Sum between two Normal objects
+    public static Normal operator +(Normal n1, Normal n2) => new Normal(n1.X + n2.X, n1.Y + n2.Y, n1.Z + n2.Z);
+
+    // Difference between two Normal objects
+    public static Normal operator -(Normal n1, Normal n2) => new Normal(n1.X - n2.X, n1.Y - n2.Y, n1.Z - n2.Z);
+
+    // Product between scalar (float) and Normal
+    public static Normal operator *(float f, Normal n) => new Normal(f * n.X, f * n.Y, f * n.Z);
+
+    // Negation
+    public static Normal operator -(Normal n)
+    {
+        return new Normal(-n.X, -n.Y, -n.Z);
+    }
+
+    // Dot product between Vec and Normal
+    public float Dot(Vec v) => X * v.X + Y * v.Y + Z * v.Z;
+
+    // Cross product between Vec and Normal
+    public Vec Cross(Vec v) => new Vec(Y * v.Z - Z * v.Y,
+        Z * v.X - X * v.Z,
+        X * v.Y - Y * v.X);
+
+    // Cross product between two Normal
+    public Normal Cross(Normal other) => new Normal(Y * other.Z - Z * other.Y,
+        Z * other.X - X * other.Z,
+        X * other.Y - Y * other.X);
+
+    // Squared norm
+    public float SquaredNorm() => (X * X) + (Y * Y) + (Z * Z);
+
+    // Norm
+    public float Norm() => MathF.Sqrt(SquaredNorm());
+
+    public void Normalize()
+    {
+        float norm = Norm();
+        X /= norm;
+        Y /= norm;
+        Z /= norm;
+    }
+
+    public override string ToString() => $"Normal<X:{X}, Y:{Y}, Z:{Z}>";
 }
--- Trace/Point.cs
+++ Trace/Point.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Trace;
 
 /// <summary>
@@ -6,8 +8,31 @@ namespace Trace;
 public struct Point
 {
     public float X, Y, Z;
-    
+
     // Constructor
     public Point(float x, float y, float z) => (X, Y, Z) = (x, y, z);
+
+    // Difference between two Points objects
+    public static Vec operator -(Point p1, Point p2) => new Vec(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+
+    // Sum between a Point and a Vec
+    public static Point operator +(Point p, Vec v) => new Point(p.X + v.X, p.Y + v.Y, p.Z + v.Z);
+
+    // Difference between a Point and a Vec
+    public static Point operator -(Point p, Vec v) => new Point(p.X - v.X, p.Y - v.Y, p.Z - v.Z);
     
+    // Product between scalar (float) and Point
+    public static Point operator *(float f, Point p) => new Point(f * p.X, f * p.Y, f * p.Z);
+
+    // Conversion from Point to Vec
+    public Vec ToVec() => new Vec(X, Y, Z);
+
+    /// <summary>
+    /// Check if two <c>Point</c> have the same coordinates with tolerance <c>epsilon</c>.
+    /// </summary>
+    public static bool CloseEnough(Point p1, Point p2, float epsilon = 1e-5f) =>
+        Utils.CloseEnough(p1.X, p2.X, epsilon) && Utils.CloseEnough(p1.Y, p2.Y, epsilon) &&
+        Utils.CloseEnough(p1.Z, p2.Z, epsilon);
+    
+    public override string ToString() => $"Point<X:{X}, Y:{Y}, Z:{Z}>";
 }
