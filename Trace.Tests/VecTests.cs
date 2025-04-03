9c884b75acab97ecb426d8594390d3c9eac540a6
Cristian <cristiancrespi1@gmail.com>
2025-04-03 11:43:20 +0200

    feat: add Vec methods and related tests

---
+++ Trace.Tests/VecTests.cs
@@ -0,0 +1,33 @@
+namespace Trace.Tests;
+
+public class VecTests
+{
+    // Test Vec constructor and CloseEnough
+    [Fact]
+    public void TestCloseEnough()
+    {
+        var v1 = new Vec(1, 2, 3);
+        var v2 = new Vec(4, 6, 8);
+
+        Assert.True(Vec.CloseEnough(v1, v1));
+        Assert.False(Vec.CloseEnough(v1, v2));
+    }
+
+    // Test Vec operations
+    [Fact]
+    public void TestOperations()
+    {
+        var v1 = new Vec(1.0f, 2.0f, 3.0f);
+        var v2 = new Vec(4.0f, 6.0f, 8.0f);
+
+        Assert.True(Vec.CloseEnough(-v1, new Vec(-1.0f, -2.0f, -3.0f)));
+        Assert.True(Vec.CloseEnough(v1 + v2, new Vec(5.0f, 8.0f, 11.0f)));
+        Assert.True(Vec.CloseEnough(v2 - v1, new Vec(3.0f, 4.0f, 5.0f)));
+        Assert.True(Vec.CloseEnough(2 * v1, new Vec(2.0f, 4.0f, 6.0f)));
+        Assert.True(Utils.CloseEnough(v1.Dot(v2), 40.0f));
+        Assert.True(Vec.CloseEnough(v1.Cross(v2), new Vec(-2.0f, 4.0f, -2.0f)));
+        Assert.True(Vec.CloseEnough(v2.Cross(v1), new Vec(2.0f, -4.0f, 2.0f)));
+        Assert.True(Utils.CloseEnough(v1.SquaredNorm(), 14.0f));
+        Assert.True(Utils.CloseEnough(v1.Norm(), MathF.Sqrt(14.0f)));
+    }
+}
--- Trace/Vec.cs
+++ Trace/Vec.cs
@@ -6,7 +6,55 @@ namespace Trace;
 public struct Vec
 {
     public float X, Y, Z;
-    
+
     // Constructor
     public Vec(float x, float y, float z) => (X, Y, Z) = (x, y, z);
+
+    // Sum between two Vec objects
+    public static Vec operator +(Vec v1, Vec v2) => new Vec(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+
+    // Difference between two Vec objects
+    public static Vec operator -(Vec v1, Vec v2) => new Vec(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+
+    // Product between scalar (float) and Vec
+    public static Vec operator *(float f, Vec v) => new Vec(f * v.X, f * v.Y, f * v.Z);
+    
+    // Negation
+    public static Vec operator -(Vec v)
+    {
+        return new Vec(-v.X, -v.Y, -v.Z);
+    }
+
+    // Dot product between two Vec
+    public float Dot(Vec other) => X * other.X + Y * other.Y + Z * other.Z;
+
+    // Cross product between two Vec
+    public Vec Cross(Vec other) => new Vec(Y * other.Z - Z * other.Y,
+        Z * other.X - X * other.Z,
+        X * other.Y - Y * other.X);
+
+    // Squared norm
+    public float SquaredNorm() => (X * X) + (Y * Y) + (Z * Z);
+
+    // Norm
+    public float Norm() => MathF.Sqrt(SquaredNorm());
+
+    public void Normalize()
+    {
+        float norm = Norm();
+        X /= norm;
+        Y /= norm;
+        Z /= norm;
+    }
+
+    public Normal ToNorm() => new Normal(X, Y, Z);
+
+    /// <summary>
+    /// Check if two <c>Vec</c> have the same components with tolerance <c>epsilon</c>.
+    /// </summary>
+    public static bool CloseEnough(Vec v1, Vec v2, float epsilon = 1e-5f) =>
+        Utils.CloseEnough(v1.X, v2.X, epsilon) && Utils.CloseEnough(v1.Y, v2.Y, epsilon) &&
+        Utils.CloseEnough(v1.Z, v2.Z, epsilon);
+
+    public override string ToString() => $"Vec<X:{X}, Y:{Y}, Z:{Z}>";
 }
