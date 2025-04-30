11c06d4d69fbf6ca2e6721ef25356412d1127067
giorgiaiori <giorgia.iori@studenti.unimi.it>
2025-04-30 12:06:56 +0200

    Implement ImageTracerTests for bug #3

--- Trace.Tests/ImageTracerTests.cs
+++ Trace.Tests/ImageTracerTests.cs
@@ -2,18 +2,31 @@ namespace Trace.Tests;
 
 public class ImageTracerTests
 {
-    // Test FireRay and FireAllRays
-    [Fact]
-    void TestImageTracer()
+    private readonly HdrImage image;
+    private readonly PerspectiveCamera camera;
+    private readonly ImageTracer tracer;
+    
+    // Setup
+    public ImageTracerTests()
     {
-        var image = new HdrImage(width: 4, height: 2);
-        var camera = new PerspectiveCamera(aspectRatio: 2);
-        var tracer = new ImageTracer(image: image, camera: camera);
-
+        image = new HdrImage(width: 4, height: 2);
+        camera = new PerspectiveCamera(aspectRatio: 2);
+        tracer = new ImageTracer(image: image, camera: camera);
+    }
+    
+    // Test (u,v) sub-mapping
+    [Fact]
+    void TestSubMapping()
+    {
         var ray1 = tracer.FireRay(0, 0, 2.5f, 1.5f);
         var ray2 = tracer.FireRay(2, 1, 0.5f, 0.5f);
         Assert.True(Ray.CloseEnough(ray1, ray2));
-
+    }
+    
+    // Test image coverage
+    [Fact]
+    void TestImageCoverage()
+    {
         tracer.FireAllRays(ray => new Color(1.0f, 2.0f, 3.0f));
 
         for (int row = 0; row < image.Height; row++)
@@ -23,5 +36,18 @@ public class ImageTracerTests
                 Assert.Equal(new Color(1.0f, 2.0f, 3.0f), image.GetPixel(col, row));
             }
         }
+    }
+    
+    // Test orientation
+    [Fact]
+    void TestOrientation()
+    {
+        // Fire ray against top-left corner of the screen
+        var topLeftRay = tracer.FireRay(0, 0, 0.0f, 0.0f);
+        Assert.True(Point.CloseEnough(new Point(0.0f, 2.0f, 1.0f), topLeftRay.At(1.0f)));
+        
+        // Fire ray against bottom-right corner of the screen
+        var bottomRightRay = tracer.FireRay(3, 1, 1.0f, 1.0f);
+        Assert.True(Point.CloseEnough(new Point(0.0f, -2.0f, -1.0f), bottomRightRay.At(1.0f)));
     }
 }
