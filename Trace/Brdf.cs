0dcd62fe554f17210033763270cb83073607c2d1
Cristian <cristiancrespi1@gmail.com>
2025-05-29 10:35:24 +0200

    fix: clean up Brdf code and increase allowance in SpecularBrdf

--- Trace/Brdf.cs
+++ Trace/Brdf.cs
@@ -14,18 +14,6 @@ public class Brdf
     }
 
     /// <summary>
-    /// Evaluate the BRDF for a specific pixel on a given surface.
-    /// </summary>
-    /// <param name="normal"><c>Normal</c> of the surface at point of 2D coordinates (u,v).</param>
-    /// <param name="inDir"><c>Vec</c> representing the direction of the incident ray on the surface
-    /// at point of 2D coordinates (u,v).</param>
-    /// <param name="outDir"><c>Vec</c> representing the direction of the diffused ray from the surface
-    /// at point of 2D coordinates (u,v).</param>
-    /// <param name="uv"><c>Vec2D</c> representing coordinates of the pixel on the surface.</param>
-    /// <returns></returns>
-    public virtual Color Eval(Normal normal, Vec inDir, Vec outDir, Vec2D uv) => Color.Black;
-
-    /// <summary>
     /// Scatter a ray from a surface.
     /// </summary>
     /// <param name="pcg"><c>Pcg</c> random number generator.</param>
@@ -43,27 +31,9 @@ public class Brdf
 /// </summary>
 public class DiffuseBrdf : Brdf
 {
-    public float Reflectance;
-
     // Constructor passing a pigment and a reflectance
-    public DiffuseBrdf(Pigment? pigment = null, float reflectance = 1.0f) : base(pigment)
+    public DiffuseBrdf(Pigment? pigment = null) : base(pigment)
     {
-        Reflectance = reflectance;
-    }
-
-    /// <summary>
-    /// Evaluate the diffuse BRDF for a specific pixel on a given surface.
-    /// </summary>
-    /// <param name="normal"><c>Normal</c> of the surface at point of 2D coordinates (u,v).</param>
-    /// <param name="inDir"><c>Vec</c> representing the direction of the incident ray on the surface
-    /// at point of 2D coordinates (u,v).</param>
-    /// <param name="outDir"><c>Vec</c> representing the direction of the diffused ray from the surface
-    /// at point of 2D coordinates (u,v).</param>
-    /// <param name="uv"><c>Vec2D</c> representing coordinates of the pixel on the surface.</param>
-    /// <returns></returns>
-    public override Color Eval(Normal normal, Vec inDir, Vec outDir, Vec2D uv)
-    {
-        return (float)(Reflectance / Math.PI) * Pigment.GetColor(uv);
     }
 
     /// <summary>
@@ -97,27 +67,8 @@ public class SpecularBrdf : Brdf
     public float ThresholdAngleRad;
 
     // Constructor
-    public SpecularBrdf(Pigment? pigment = null, float? thresholdAngleRad = null) : base(pigment)
+    public SpecularBrdf(Pigment? pigment = null) : base(pigment)
     {
-        ThresholdAngleRad = thresholdAngleRad ?? MathF.PI / 1800.0f;
-    }
-
-    /// <summary>
-    /// Evaluate the perfectly reflective BRDF for a specific pixel on a given surface.
-    /// </summary>
-    /// <param name="normal"><c>Normal</c> of the surface at point of 2D coordinates (u,v).</param>
-    /// <param name="inDir"><c>Vec</c> representing the direction of the incident ray on the surface
-    /// at point of 2D coordinates (u,v).</param>
-    /// <param name="outDir"><c>Vec</c> representing the direction of the reflected ray from the surface
-    /// at point of 2D coordinates (u,v).</param>
-    /// <param name="uv"><c>Vec2D</c> representing coordinates of the pixel on the surface.</param>
-    /// <returns></returns>
-    public override Color Eval(Normal normal, Vec inDir, Vec outDir, Vec2D uv)
-    {
-        float thetaIn = MathF.Acos(normal.Dot(inDir));
-        float thetaOut = MathF.Acos(normal.Dot(outDir));
-        if (MathF.Abs(thetaIn - thetaOut) < ThresholdAngleRad) return Pigment.GetColor(uv);
-        return Color.Black;
     }
 
     /// <summary>
@@ -135,7 +86,7 @@ public class SpecularBrdf : Brdf
         float dotProd = normal.Dot(incomingDir);
 
         return new Ray(origin: interactionPoint,
-            dir: incomingDir - 2.0f * dotProd * normal.ToVec(), tmin: 1e-5f,
+            dir: incomingDir - 2.0f * dotProd * normal.ToVec(), tmin: 1e-3f,
             tmax: float.PositiveInfinity, depth: depth);
     }
 }
